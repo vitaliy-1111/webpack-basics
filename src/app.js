79c70e0a9d90161fe0b396f7b2dd5ac57e98a78c
Vitaliy <networkvetalius@gmail.com>
2022-01-13 14:58:24 +0200

    add dependencies

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import hello from './hello.js';
+import "./scss/common.scss";
+
+hello('Dolly');
+console.log('hi');
+console.log('my first webpack');
+console.log('update')
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'myfirst.boundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+       {
+        test: /\.scss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+       
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // title: "Webpack Basics",
+      template: "src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+
+      },
+    ),
+  ],
+
+  // devServer: {
+  //   open: "true",
+  // }
+};
